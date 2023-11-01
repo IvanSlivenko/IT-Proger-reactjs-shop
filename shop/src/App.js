9a489c8724eb40cb22db3fb8313da43749a1577c
IvanSlivenko <ivan8822@ulr.net>
2023-11-01 20:56:56 +0200

    01-11-2023-20-56

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,62 +2,79 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer"
 import Items from "./components/items";
+// import test from "../public/img/21066_6686646.jpg";
+// import test from "../public/img/nut-tree-07-1000x1000.jpg";
+// import test from "../public/img/oxford.jpg";
+// import test from "../public/img/BanerFurniture.jpg";
+
 
 
 class App extends React.Component {
-  constructor(props) { 
+  constructor(props) {
     super(props)
     this.state = {
+      orders: [],
       items: [
         {
           id: 1,
           title: "товар 1",
-          img: "img-1",
+          img: "21066_6686646.jpg",
           desc: "описа товару 1",
           price: "49,99",
         },
         {
           id: 2,
           title: "товар 2",
-          img: "img-2",
+          img: "nut-tree-07-1000x1000.jpg",
           desc: "описа товару 2",
           price: "149,99",
         },
         {
           id: 3,
           title: "товар 3",
-          img: "img-3",
+          img: "oxford.jpg",
           desc: "описа товару 3",
           price: "549,99",
         },
         {
           id: 4,
           title: "товар 4",
-          img: "img-4",
+          img: "BanerFurniture.jpg",
           desc: "описа товару 4",
           price: "1549,99",
         },
         {
           id: 5,
           title: "товар 5",
-          img: "img-5",
+          img: "shafa_loft_2dg.jpg",
           desc: "описа товару 5",
           price: "2549,99",
-        }
+        },
+        {
+          id: 6,
+          title: "товар 6",
+          img: "comod.jpg",
+          desc: "описа товару 6",
+          price: "1800,99",
+        },
       ],
     };
+    this.addToOrder = this.addToOrder.bind(this)
   }
-  render() { 
-  return (
-    <div className="wrapper">
-      <Header />
-      <Items items={this.state.items} />
-      <Footer />
-    </div>
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
     )
   }
-}
 
+  addToOrder(item) {
+    this.setState({ orders: [...this.state.orders, item] })
+  }
+}
 export default App;
 
  
--- shop/src/components/item.js
+++ shop/src/components/item.js
@@ -3,15 +3,14 @@ import React, { Component } from 'react'
 export class Item extends Component {
   render() {
     return (
-        <div className='item'>
-            <img src={""} />
-            <h2>{this.props.item.title}</h2>
-            <p>{this.props.item.desc}</p>
-            <b>{this.props.item.price}</b>
-            <div className='add-to-cart'>+</div>
-            
-        </div>
-    )
+      <div className="item">
+        <img src={"./img/" + this.props.item.img} />
+        <h2>{this.props.item.title}</h2>
+        <p>{this.props.item.desc}</p>
+        <b>{this.props.item.price} грн</b>
+        <div className="add-to-cart">+</div>
+      </div>
+    );
   }
 }
 
